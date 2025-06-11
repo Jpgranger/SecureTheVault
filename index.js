13519b709de747186714b974f034ae29563828b0
Jpgranger <joey1027@live.com>
2025-06-11 12:18:43 -0500

    LALALA

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Secure The Vault");
+let code1;
+code1 = 7+3;
+// console.log(code1);
+
+let code2 = 82/2-1;
+// console.log(code2);
+
+let code3 = 80%41;
+// console.log(code3);
+
+let message = "The vault has been secured. The combination is:";
+// console.log(message);
+
+let CodeA = code1 - code2 - code3;
+// console.log(CodeA)
+
+let CodeB = `${code1}-${code2}-${code3}`;
+// console.log(CodeB)
+
+console.log(message, CodeA, CodeB)
